1774ead207743ee25e61b5333a28dd1836b07c27
xiao kang <Augustles@users.noreply.github.com>
2016-04-07 15:09:26 +0800

    Update my_custom.sh

--- conf/my_custom.sh
+++ conf/my_custom.sh
@@ -3,7 +3,7 @@ sudo apt-get update
 sudo apt-get -y install zsh vim git python python-pip wget curl
 sudo apt-get -y install python-dev
 sudo apt-get install -y libevent-dev libxml2-dev libxslt1-dev
-sudo apt-get install -y libssl-dev libcurl4-openssl-dev python-dev
+sudo apt-get install -y libssl-dev libcurl4-openssl-dev python-devs libffi-dev
 sudo apt-get install -y build-essential autoconf libtool pkg-config python-opengl python-imaging python-pyrex python-pyside.qtopengl idle-python2.7 qt4-dev-tools qt4-designer libqtgui4 libqtcore4 libqt4-xml libqt4-test libqt4-script libqt4-network libqt4-dbus python-qt4 python-qt4-gl libgle3
 sudo pip install scrapy
 sudo pip install pyOpenSSL==0.13
