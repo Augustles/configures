82267ad39aba8182bebcfcc701558ddab644fc98
Augutst <Augutst@Augutsts-MacBook-Pro.local>
2017-01-12 19:42:59 +0800

    update

--- stackflow/yua.py
+++ stackflow/yua.py
@@ -19,7 +19,6 @@ with open('yua.txt', 'r') as f:
 
 npks = set()
 
-os.chdir(os.getcwd() + '/yua3/')
 headers = {
     'UserAgent': 'Googlespider',
 }
@@ -51,16 +50,13 @@ def worker(qs, first=True):
         r = requests.get(qs, headers=headers, verify=True)
         soup = bs(r.content, 'lxml')
         info = soup.find('div', attrs={'class': 'stream'}).find_all('li', attrs={'data-item-type': 'tweet'})
-        next_page = info[-1].get('data-item-id', '')
-        yield worker(next_page, first=False)
+        # next_page = info[-1].get('data-item-id', '')
+        # yield worker(next_page, first=False)
 
     for x in info:
         try:
-            # text = x.find('div', attrs={'class': 'js-tweet-text-container'}).text
             imgs = x.find_all('img', attrs={'data-aria-label-part': True})
             videos = x.find('div', attrs={'class': 'AdaptiveMedia-video'})
-            # if len(text) > 10:
-                # text = text[:11]
             if imgs:
                 for y in imgs:
                     try:
@@ -108,31 +104,15 @@ def worker(qs, first=True):
             with open('yua.log', 'a') as f:
                 f.writelines(e)
 
-    raise gen.Return(npks)
 
 
 @gen.coroutine
-def nstart(qs, first=True):
+def worker2(qs, first=True):
     if not first:
-        cmd = "curl 'https://www.instagram.com/query/' -H \
-            'cookie: mid=mid=WHSEXwAEAAFmyVFoJ_n7bZ5eD9VE; ig_pr=2; ig_vw=1276; \
-            s_network=""; csrftoken=8Fz2hZOeV0kXQk5tFJkVgUzqcnIAUi7e' -H \
-            'origin: https://www.instagram.com' -H 'accept-encoding: gzip, deflate, br' -H \
-            'accept-language: en-US,en;q=0.8' -H \
-            'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.95 Safari/537.36' -H \
-            'x-requested-with: XMLHttpRequest' -H 'x-csrftoken: 8Fz2hZOeV0kXQk5tFJkVgUzqcnIAUi7e' -H 'x-instagram-ajax: 1' -H \
-            'content-type: application/x-www-form-urlencoded' -H 'accept: */*' -H \
-            'referer: https://www.instagram.com/yua_mikami/' -H 'authority: www.instagram.com' --data \
-            'q=ig_user(3932298284)+%7B+media.after({0}%2C+12)+%7B%0A++count%2C%0A++nodes+%7B%0A\
-            ++++caption%2C%0A++++code%2C%0A++++comments+%7B%0A++++++count%0A++++%7D%2C%0A++++comments_disabled%2C%0A++++\
-            date%2C%0A++++dimensions+%7B%0A++++++height%2C%0A++++++width%0A++++%7D%2C%0A++++display_src%2C%0A++++id%2C%0A++++\
-            is_video%2C%0A++++likes+%7B%0A++++++count%0A++++%7D%2C%0A++++owner+%7B%0A++++++id%0A++++%7D%2C%0A++++thumbnail_src%2C%0A++++\
-            video_views%0A++%7D%2C%0A++page_info%0A%7D%0A+%7D&ref=users%3A%3Ashow&query_id=17846611669135658' --compressed > test.txt".format(qs)
         cmd = '''curl 'https://www.instagram.com/query/' -H 'cookie: mid=WHSEXwAEAAFmyVFoJ_n7bZ5eD9VE; ig_pr=2; ig_vw=1276; s_network=""; csrftoken=8Fz2hZOeV0kXQk5tFJkVgUzqcnIAUi7e' -H 'origin: https://www.instagram.com' -H 'accept-encoding: gzip, deflate, br' -H 'accept-language: en-US,en;q=0.8' -H 'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.95 Safari/537.36' -H 'x-requested-with: XMLHttpRequest' -H 'x-csrftoken: 8Fz2hZOeV0kXQk5tFJkVgUzqcnIAUi7e' -H 'x-instagram-ajax: 1' -H 'content-type: application/x-www-form-urlencoded' -H 'accept: */*' -H 'referer: https://www.instagram.com/mikami_yua/' -H 'authority: www.instagram.com' --data 'q=ig_user(3932298284)+%7B+media.after({0}%2C+12)+%7B%0A++count%2C%0A++nodes+%7B%0A++++caption%2C%0A++++code%2C%0A++++comments+%7B%0A++++++count%0A++++%7D%2C%0A++++comments_disabled%2C%0A++++date%2C%0A++++dimensions+%7B%0A++++++height%2C%0A++++++width%0A++++%7D%2C%0A++++display_src%2C%0A++++id%2C%0A++++is_video%2C%0A++++likes+%7B%0A++++++count%0A++++%7D%2C%0A++++owner+%7B%0A++++++id%0A++++%7D%2C%0A++++thumbnail_src%2C%0A++++video_views%0A++%7D%2C%0A++page_info%0A%7D%0A+%7D&ref=users%3A%3Ashow&query_id=17846611669135658' --compressed > test.txt'''.format(qs)
         os.system(cmd)
         with open('test.txt', 'r') as f:
             out = f.readlines()
-        # soup = bs(out, 'lxml')
         soup = json.loads(out[0])
         try:
             next_page = soup['media']['page_info']['end_cursor']
@@ -145,10 +125,7 @@ def nstart(qs, first=True):
         r.get(qs)
         soup = bs(r.page_source, 'lxml')
         r.quit()
-        next_page = '1378701645453631542'
-        print next_page
         info = soup.find('div', attrs={'class': '_nljxa'}).find_all('img', attrs={'src': True})
-        yield nstart(next_page, first=False)
     for x in info:
         try:
             try:
@@ -157,7 +134,8 @@ def nstart(qs, first=True):
                 img = x.get('src', '').split('?')[0]
             img = img.replace('/s640x640', '')
             r = yield downloader(img)
-            fn = img.split('/')[-1]
+            fn = img.split('/')[-1].split('_')[-2] + '.jpg'
+            print fn
             content = r.content
             res = gen_md5(content)
             pk = res.result()
@@ -174,9 +152,15 @@ def nstart(qs, first=True):
 
 @gen.coroutine
 def main():
-    start = 'https://twitter.com/yua_mikami'
-    start = 'https://www.instagram.com/mikami_yua/'
-    npks = yield nstart(start)
+    start = ['https://twitter.com/yua_mikami', 'https://www.instagram.com/mikami_yua/', 'https://www.instagram.com/yua_mikami/']
+    for x in start:
+        if 'twitter' in x:
+            os.chdir(os.getcwd() + 'test2/twiyua')
+            yield worker(x)
+        elif 'instagram' in x:
+            os.chdir(os.getcwd() + 'test2/insyua')
+            yield worker2(x)
+
 
 if __name__ == '__main__':
     st = time()
