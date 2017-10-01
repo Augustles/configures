a548a79ba0ff8e064f5bd636aa617b820c3e5c16
August <Augutst@August-3.local>
2017-10-01 08:40:02 +0800

    u

--- stackflow/ck101.py
+++ stackflow/ck101.py
@@ -46,7 +46,17 @@ def parse_page(url):
         gen.Sleep(3)
         yield parse_url(url)
     if res.status_code == 200:
-        raise gen.Return(res)
+        info = {}
+        soup = bs(res.content, 'lxml')
+        article = soup.find('article')
+        if article.find('span', attrs={'class': 'postDateLine'}):
+            info['create_time_str'] = article.find('span', attrs={'class': 'postDateLine'}).text.strip()
+        if article.find('a', attrs={'class': 'authorName'}):
+            info['username'] = article.find('a', attrs={'class': 'authorName'}).get('title', '').strip()
+            info['user_space'] = 'https://ck101.com' + article.find('a', attrs={'class': 'authorName'}).get('href', '')
+        if article.find('tbody'):
+            info['html3'] = str(article.find('tbody'))
+        raise gen.Return(info)
     else:
         gen.Sleep(3)
         yield parse_page(url)
@@ -81,7 +91,8 @@ def main():
         url = x.get('url', '')
         if url:
             res = yield parse_page(url)
-            db.update({'url': url, 'category': x['category']}, {'$set': {'html': res.content}})
+            x.update(res)
+            db.update( {'url': url, 'category': x['category']}, {'$set': x} )
 
 if __name__ == '__main__':
     st = time()
